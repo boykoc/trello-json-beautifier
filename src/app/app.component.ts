928836dc8bb3c7660363df5b81b6fbf6b57532e4
Cody Boyko <codyboyko@yahoo.ca>
2017-05-26 16:36:09 +0000

    Read and display uploaded JSON file.
    Use File API to read in file use has selected from their machine.
    Add new TrelloBoard class to store the data and bind to view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export class TrelloBoard {
+  data: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +11,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Trello JSON Export Beautifier';
-  trello_json = {}; // Put your trello json export data here.
+  trello_json: TrelloBoard = {
+    data: {}
+  };
+  
+  file: File;
+  onChange(event) {
+    // Get the file when the input changes.
+    this.file = event.srcElement.files[0];
+    // Create a new file reader to read the file.
+    var reader = new FileReader();
+    // When the reader is loaded and ready, spit out the file content as json.
+    // capture outer this and and pass inside. () => 
+    reader.onload = () => {
+      this.trello_json.data = JSON.parse(reader.result);
+      console.log(this.trello_json.data)
+    }
+    // Read the file as text.
+    reader.readAsText(this.file);
+  }
 }
